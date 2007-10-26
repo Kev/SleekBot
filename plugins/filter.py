99f88f96dabd6dc56fe720dd18df0eddd933bb38
Kevin Smith <git@kismith.co.uk>
2007-10-26 13:00:54 +0000

    Early filter I was working on last night.
    git-svn-id: svn://netflint.net/sleekbot@48 9f595f45-9794-4242-9c30-1c0c50310c13

---
+++ plugins/filter.py
@@ -0,0 +1,83 @@
+"""
+    filter.py - A SleekBot plugin to filter text.
+    Copyright (C) 2007 Kevin Smith
+
+    SleekBot is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    SleekBot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this software; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import logging
+import re
+import random
+from xml.etree import ElementTree as ET
+
+class leetFilter():
+    def __init__(self):
+        self.mappings = {
+            'a':['4','/\\','@','/-\\','^'],
+            'b':[],
+            'c':[],
+            'd':[],
+            'e':[],
+            'f':[],
+            'g':[],
+            'h':[],
+            'i':[],
+            'j':[],
+            'k':[],
+            'l':[],
+            'm':[],
+            'n':[],
+            'o':[],
+            'p':[],
+            'q':[],
+            'r':[],
+            's':[],
+            't':[],
+            'u':[],
+            'v':[],
+            'w':[],
+            'x':[],
+            'y':[],
+            'z':['2','~/_','%','>_','7_']
+        }
+        pass
+    
+    def filter(self, text):
+        for i in range(len(text)):
+            leets = self.mappings.get(text[i], [])
+            if len(leets) == 0:
+                continue
+            text = text[:i] + leets[random.randint(0, len(leets) - 1)] + text[i+1:]
+        return text
+            
+
+class filter(object):
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.config = config
+        self.bot.addIMCommand('filter', self.handle_filter)
+        self.bot.addMUCCommand('filter', self.handle_filter)
+        self.bot.addHelp('filter', 'Text filter command', "Parses the text through a filter.", 'filter filtertype text')
+        self.availableFilters = {}
+        self.availableFilters['leet'] = leetFilter()
+
+    def handle_filter(self, command, args, msg):
+        if args == None or args == "" or len(args.split(" ")) < 2:
+            return "Insufficient information, please check help."
+        language = args.split(" ")[0].lower()
+        text = " ".join(args.split(" ")[1:])
+        if language not in self.availableFilters.keys():
+            return "Language %s not available" % language
+        return self.availableFilters[language].filter(text)
