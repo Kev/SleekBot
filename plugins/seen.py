9245e17c185db360085ec757f4f3716cecb8d3c8
Kevin Smith <git@kismith.co.uk>
2008-04-18 15:36:39 +0000

    committing a quick edit to save full jids - untested
    git-svn-id: svn://netflint.net/sleekbot@122 9f595f45-9794-4242-9c30-1c0c50310c13

--- plugins/seen.py
+++ plugins/seen.py
@@ -38,6 +38,61 @@ class seenevent(object):
         self.stanzaType = stanzaType
         self.text = text
 
+class jidevent(object):
+    """ Represent the last seen jid of a user.
+    """
+    def __init__(self, muc, nick, jid, eventTime):
+        """Create event"""
+        self.muc = muc
+        self.nick = nick
+        self.jid = jid
+        self.eventTime = eventTime
+
+class jidstore(object):
+    def __init__(self, store):
+        self.store = store
+        self.createTable()
+
+    def createTable(self):
+        db = self.store.getDb()
+        if not len(db.execute("pragma table_info('whowas')").fetchall()) > 0:
+            db.execute("""CREATE TABLE whowas (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT, muc VARCHAR(256),
+                       nick VARCHAR(256), jid VARCHAR(256), eventTime DATETIME)""")
+        db.close()
+
+    def update(self, event):
+        db = self.store.getDb()
+        cur = db.cursor()
+        logging.debug("Updating whowas")
+        cur.execute('SELECT * FROM whowas WHERE nick=? AND muc=?', (event.nick,event.muc))
+        if (len(cur.fetchall()) > 0):
+            cur.execute('UPDATE whowas SET jid=?, eventTime=?', (event.jid, event.eventTime))
+            logging.debug("Updated existing whowas")
+        else:
+            cur.execute('INSERT INTO whowas(nick, muc, jid, eventTime) VALUES(?,?,?,?)',(event.nick, event.muc, event.jid, event.eventTime))
+            logging.debug("Added new whowas")
+        db.commit()
+        db.close()
+
+
+    def get(self, nick, muc):
+        db = self.store.getDb()
+        cur = db.cursor()
+        cur.execute('SELECT * FROM seen WHERE nick=? AND muc=?', (nick,muc))
+        results = cur.fetchall()
+        if len(results) == 0:
+            return None
+        return jidevent(results[0][1],results[0][2],results[0][3],datetime.datetime.strptime(results[0][4][0:19],"""%Y-%m-%d %H:%M:%S""" ))
+        db.close()
+
+    def delete(self, nick, muc):
+        db = self.store.getDb()
+        cur = db.cursor()
+        cur.execute('DELETE FROM seen WHERE nick=? AND muc=?', (nick,muc))
+        db.commit()
+        db.close()
+
 class seenstore(object):
     def __init__(self, store):
         #self.null = None
@@ -53,6 +108,8 @@ class seenstore(object):
             db.execute("""CREATE TABLE seen (
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        nick VARCHAR(256), eventTime DATETIME, muc VARCHAR(256), stanzaType INTEGER, text VARCHAR(256))""")
+        #if len(db.execute("pragma table_info('seen')").fetchall()) == 6:
+        #    db.execute("""ALTER TABLE seen ADD COLUMN fullJid VARCHAR(256)""")
         db.close()
     
     def update(self, event):
@@ -96,6 +153,9 @@ class seen(object):
         self.bot.addIMCommand('seen', self.handle_seen_request)
         self.bot.addMUCCommand('seen', self.handle_seen_request)
         self.bot.addHelp('seen', 'Last seen Command', "See when a user was last seen", 'seen')
+        self.bot.addIMCommand('whowas', self.handle_whowas_request)
+        self.bot.addMUCCommand('whowas', self.handle_whowas_request)
+        self.bot.addHelp('whowas', 'Jid of member', "See the last jid of a member", 'whowas')
         self.started = datetime.timedelta(seconds = time.time())
         self.bot.add_event_handler("groupchat_presence", self.handle_groupchat_presence, threaded=True)
         self.bot.add_event_handler("groupchat_message", self.handle_groupchat_message, threaded=True)
@@ -109,6 +169,8 @@ class seen(object):
         else:
             pType = seenevent.presenceType
         self.seenstore.update(seenevent(presence['nick'], presence['dateTime'], presence['room'], pType, presence.get('status', None)))
+        self.jidstore.update(jidevent(presence['nick'], presence['room'], self.bot.getRealJid(presence['jid']) , presence['dateTime']))
+        
         
     
     def handle_groupchat_message(self, message):
@@ -118,6 +180,8 @@ class seen(object):
             return
         message['dateTime'] = datetime.datetime.now()
         self.seenstore.update(seenevent(message['name'], message['dateTime'], message['room'], seenevent.messageType, message['message']))
+        self.jidstore.update(jidevent(message['name'], message['room'], self.bot.getRealJidFromMessag(message), message['dateTime']))
+        
     
     def handle_seen_request(self, command, args, msg):
         if args == None or args == "":
